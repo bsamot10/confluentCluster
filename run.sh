1ec96ccd51c424abb6960302aada185d08254dfb
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2024-01-11 18:35:42 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ docker run -d \
         --name confluent-<node_id> \
         --ip 10.0.1.1<node_id> \
         --network overlay-cluster \
+	--restart unless-stopped \
         -it bsamot10/confluent-cluster
 	
 . myid.sh
