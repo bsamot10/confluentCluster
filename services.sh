9842d3bc505b735acc2df39449a3bfacf281d82d
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2024-01-11 20:21:28 +0200

    Update services.sh

--- services.sh
+++ services.sh
@@ -1,10 +1,10 @@
 docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-zookeeper.sh"
-sleep 20
+sleep 10
 docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-kafka.sh"
 . cleanup.sh
-#sleep 20
-#docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-schema.sh"
-#sleep 20
-#docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-connect.sh"
-#sleep 20
+sleep 10
+docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-schema.sh"
+sleep 10
+docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-connect.sh"
+#sleep 10
 #docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-ksql.sh"
