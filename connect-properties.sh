93c57a629c42f8f88ab1b11e99dd0931df08ddbb
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2024-01-11 16:56:56 +0200

    Update connect-properties.sh

--- connect-properties.sh
+++ connect-properties.sh
@@ -1,1 +1,1 @@
-docker cp conf/connect-distributed.properties confluent-<node_id>:/confluent/confluent-7.5.2/etc/kafka/connect-distributed.properties
+docker cp conf/connect-distributed.properties confluent-<node_id>:/usr/local/confluent/etc/kafka/connect-distributed.properties
