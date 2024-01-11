b72e5ef22528a045292068d5dc27451d5e476497
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2024-01-11 20:25:13 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,1 +1,1 @@
-docker exec confluent-<node_id>  bash -c 'for topic in {_schema_encoders,_dek_registry_keys,_schemas}; do kafka-configs --topic $topic --bootstrap-server 10.0.1.1<node_id>:9092 --add-config "cleanup.policy=compact" --alter; done;'
+docker exec confluent-<node_id>  bash -c 'for topic in {_schemas,_schema_encoders,_dek_registry_keys}; do kafka-configs --topic $topic --bootstrap-server 10.0.1.1<node_id>:9092 --add-config "cleanup.policy=compact" --alter; done;'
