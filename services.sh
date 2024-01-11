04871fd3ecf193d57e74b8ceaf2119a720e26a19
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2024-01-11 20:39:38 +0200

    Update services.sh

--- services.sh
+++ services.sh
@@ -3,24 +3,24 @@ docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . s
 sleep 20
 docker exec confluent-<node_id> jps
 
-echo "start-kafka.sh"
+echo -e "\nstart-kafka.sh"
 docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-kafka.sh"
 sleep 20
 docker exec confluent-<node_id> jps
 
-echo "cleanup.sh"
+echo -e "\ncleanup.sh"
 . cleanup.sh
 
-echo "start-schema.sh"
+echo -e "\nstart-schema.sh"
 docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-schema.sh"
 sleep 20
 docker exec confluent-<node_id> jps
 
-echo "start-connect.sh"
+echo -e "\nstart-connect.sh"
 docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-connect.sh"
 sleep 20
 docker exec confluent-<node_id> jps
 
-#echo "start-ksql.sh"
+#echo -e "\nstart-ksql.sh"
 #docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-ksql.sh"
 #docker exec confluent-<node_id> jps
