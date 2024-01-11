3f187668c31f9dadce00f541f8de859ac7c5febe
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2024-01-11 16:56:45 +0200

    Update server-properties.sh

--- server-properties.sh
+++ server-properties.sh
@@ -1,1 +1,1 @@
-docker cp conf/server.properties confluent-<node_id>:/confluent/confluent-7.5.2/etc/kafka/server.properties
+docker cp conf/server.properties confluent-<node_id>:/usr/local/confluent/etc/kafka/server.properties
