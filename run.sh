da4ab6da904258c5460b700d38895b2e237abbff
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2024-01-12 15:58:40 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ docker run -d \
         -h confluent-<node_id> \
         --name confluent-<node_id> \
         --ip 10.0.1.1<node_id> \
+	--env JMX_PORT='9060' \
         --network overlay-cluster \
 	--restart unless-stopped \
         -it bsamot10/confluent-cluster
