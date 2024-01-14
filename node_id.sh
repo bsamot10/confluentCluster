e5db22ac36ee0e676d3c9d5100a09a1d8b0d6fbd
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2024-01-14 11:47:18 +0200

    Update node_id.sh

--- node_id.sh
+++ node_id.sh
@@ -4,5 +4,5 @@ then
         exit 1;
 fi
 
-for file in {run.sh,shell.sh,services.sh,cleanup.sh,jmx.sh,conf/myid,conf/server.properties,conf/connect-distributed.properties,myid.sh,server-properties.sh,connect-properties.sh};
+for file in {run.sh,shell.sh,services.sh,old/cleanup.sh,old/jmx.sh,conf/myid,conf/server.properties,conf/connect-distributed.properties,myid.sh,server-properties.sh,connect-properties.sh};
 do sed -i "s/<node_id>/$1/g" $file; done;
