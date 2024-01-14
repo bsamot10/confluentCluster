eab0c435f8b207d7f6f68c14a0c064596ed69a47
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2024-01-14 11:21:09 +0200

    Update services.sh

--- services.sh
+++ services.sh
@@ -1,29 +1,23 @@
-#echo -e "\njmx.sh"
-#. jmx.sh
-
 echo -e "\nstart-zookeeper.sh"
 docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-zookeeper.sh"
-sleep 20
+sleep 15
 docker exec confluent-<node_id> jps
 
 echo -e "\nstart-kafka.sh"
 docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-kafka.sh"
-sleep 20
+sleep 15
 docker exec confluent-<node_id> jps
-
-#echo -e "\ncleanup.sh"
-#. cleanup.sh
 
 echo -e "\nstart-schema.sh"
 docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-schema.sh"
-sleep 20
+sleep 15
 docker exec confluent-<node_id> jps
 
 echo -e "\nstart-connect.sh"
 docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-connect.sh"
-sleep 20
+sleep 15
 docker exec confluent-<node_id> jps
 
-#echo -e "\nstart-ksql.sh"
-#docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-ksql.sh"
-#docker exec confluent-<node_id> jps
+echo -e "\nstart-ksql.sh"
+docker exec -d confluent-<node_id> bash -c "cd /usr/local/confluent/bash/ && . start-ksql.sh"
+docker exec confluent-<node_id> jps
