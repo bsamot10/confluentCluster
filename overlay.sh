31d263ab5f6b2aa468d7cecbc0dcaac632315b9f
bsamot10 <57543732+bsamot10@users.noreply.github.com>
2024-01-24 19:02:41 +0200

    Update overlay.sh

--- overlay.sh
+++ overlay.sh
@@ -1,1 +1,1 @@
-docker network create -d overlay --subnet 10.0.1.0/24 --gateway 10.0.1.1 --attachable overlay-cluster
+docker network create -d overlay --subnet 10.0.1.0/24 --gateway 10.0.1.1 --attachable confluent-net
